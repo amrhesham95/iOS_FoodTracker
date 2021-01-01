92a3f5bd64d6ff13757a9d93ca1634d498a1f01f
Amr Hesham <amrhesham9595@gmail.com>
2021-01-01 16:02:44 +0200

    Download this in the end of connect UI Section (just before starting work with view controllers )

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -7,19 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameTextField.delegate = self
     }
 
     @IBAction func setDefaultLabelText(_ sender: Any) {
         self.mealNameLabel.text = "Default Text"
     }
     
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
