b4893f4118d7493e52d3733a49d851003b814669
Amr Hesham <amrhesham9595@gmail.com>
2021-01-30 16:42:42 +0200

    2 - Create a Custom View 3 - Display the Custom View

---
+++ FoodTracker/FoodTracker/RatingControl/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Amr Hesham on 30/01/2021.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    
+}
