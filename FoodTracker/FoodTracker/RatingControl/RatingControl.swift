95344468bc1af7add4aeeb66842902ce310de6a9
Amr Hesham <amrhesham9595@gmail.com>
2021-01-30 16:51:31 +0200

    3 - Display Custom View.

--- FoodTracker/FoodTracker/RatingControl/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl/RatingControl.swift
@@ -11,11 +11,24 @@ class RatingControl: UIStackView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
     }
-
     
+
+    // MARK: - Setup Buttons
+    private func setupButtons() {
+        let button = UIButton()
+        button.backgroundColor = .red
+        
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        addArrangedSubview(button)
+        
+    }
 }
