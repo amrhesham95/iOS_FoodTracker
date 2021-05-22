986a20455eb3966ca9b2a31239762d71a349c652
Amr Hesham <amrhesham9595@gmail.com>
2021-05-22 19:52:24 +0200

    14 - deleting meal

--- FoodTracker/FoodTracker/MealsTableViewController.swift
+++ FoodTracker/FoodTracker/MealsTableViewController.swift
@@ -47,25 +47,26 @@ class MealsTableViewController: UITableViewController {
     }
   
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+          deleteMealAt(indexPath)
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+//            tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
@@ -153,5 +154,16 @@ extension MealsTableViewController {
     } catch {
       print(error.localizedDescription)
     }
+  }
+  
+  func deleteMealAt(_ indexPath: IndexPath) {
+    // delete meal from meals array
+    meals.remove(at: indexPath.row)
+    
+    // save meals array
+    saveMeals()
+    
+    // load meals array
+    loadMeals()
   }
 }
